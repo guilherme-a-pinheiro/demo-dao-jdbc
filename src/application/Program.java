0c5434bef6ff8d2aeaf59aeed5e8589e8d0ac683
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-03-29 21:50:09 -0300

    criando entity seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
-       
+        Department dp = new Department(1, "Books");
+
+        Seller seller = new Seller(1, "Name", "name@email.com", new Date(), 3000.0, dp);
+
+        System.out.println(seller);
     }
 }
