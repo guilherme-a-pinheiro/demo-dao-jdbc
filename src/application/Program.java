93daaa27ca163597e5c2a8da361302e212e55d8f
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-03-31 12:51:04 -0300

    implementando metodo update

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=========\n");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+
     }
 }
