21a88c7534c9b81dc4e8714939cbd7901694a242
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-03-31 12:17:34 -0300

    criando metodo findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,14 +6,21 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("Teste findByID");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n=========");
+        System.out.println("Teste findByDepartment");
+        Department dep = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(dep);
+        list.forEach(System.out::println);
     }
 }
