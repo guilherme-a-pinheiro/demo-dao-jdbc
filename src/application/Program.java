62e8ab81bd5f11cdc8f2e72938d3279d3935d8bb
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-03-31 12:43:17 -0300

    implementando metodo insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -26,5 +27,12 @@ public class Program {
         System.out.println("Teste findAll");
         list = sellerDao.findAll();
         list.forEach(System.out::println);
+
+        System.out.println("\n=========\n");
+        System.out.println("Teste insert");
+        Seller newSeller = new Seller(null, "Greg", "greg@email.com", new Date(), 4000.0, dep);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
     }
 }
